75940e84168247d097e0c11c7c05bd95ffb56a72
adrianvillasalte <adrianvillasalte@gmail.com>
2017-11-14 15:25:38 -0300

    Ejercicio nuevo

---
+++ Python/ex11.py
@@ -0,0 +1,13 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weight?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
+
+
+
+#Exercise 11 - Asking Questions, Learn Python the Hard Way
